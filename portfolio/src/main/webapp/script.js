8237a065310cd919d97db89e58a84f64fa88403a
Maya <mayahp@seas.upenn.edu>
2020-06-04 15:14:32 +0000

    getRandomFact() no longer uses doPost()

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -14,19 +14,18 @@
 
 function getRandomFact() {
     console.log('Getting a random fact.');
+    const facts = ['On my mom\'s side of the family, I\'m the oldest of my generation.',
+    'I have two younger sisters.', 'I once won a jigsaw puzzle competition.',
+    'After graduating, I want to volunteer as CS instructor along with my full-time job.',
+    'My entire extended family immigrated to the US.',
+    'I am the oldest of all of my immediate cousins.',
+    'My first large team coding project was done with an international team.',
+    'I\'m interested in K-Pop.', 'I grew up playing with my younger cousins on a regular basis.'];
 
-    fetch('/data')
-        .then(response => response.json())
-        .then((facts) => {
-            const factsListElement = document.getElementById('fact-container');
-            factsListElement.innerHTML = '';
-            factsListElement.appendChild(createListElement('First fact: '
-                + facts.firstFact));
-            factsListElement.appendChild(createListElement('Second fact: '
-                + facts.secondFact));
-            factsListElement.appendChild(createListElement('Third fact: '
-                + facts.thirdFact));
-    });
+    const fact = facts[Math.floor(Math.random() * facts.length)];
+
+    const factContainer = document.getElementById('fact-container');
+    factContainer.innerText = song;
 }
 
 function createListElement(text) {
