0f7ca31eb9b07875d776995cf5507b408bc8252e
Maya <mayahp@seas.upenn.edu>
2020-06-10 18:59:12 +0000

    markers

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -20,110 +20,185 @@ function start() {
 }
 
 function initMap() {
-    console.log('wtf');
+    const upenn = {
+        lat: 39.9522229,
+        lng: -75.1954024
+    };
+    const hill = {
+        lat: 39.9530122,
+        lng: -75.1928656
+    };
+    const zeta = {
+        lat: 39.9543422,
+        lng: -75.2060464
+    };
+    const eng = {
+        lat: 39.9516732,
+        lng: -75.1933924
+    };
+    const sobol = {
+        lat: 39.9510881,
+        lng: -75.2009277
+    };
+    const love = {
+        lat: 39.9517666,
+        lng: -75.1962875
+    };
+
+    const places = [hill, zeta, eng, sobol, love];
+
     const map = new google.maps.Map(
-        document.getElementById('map'),
-        {center: {
-            lat: 40.3079168, 
-            lng: -74.1408768
-        }, 
-        zoom: 16,
-        styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-                featureType: 'administrative.locality',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
-            },
-            {
-                featureType: 'poi',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'geometry',
-                stylers: [{color: '#263c3f'}]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#6b9a76'}]
-            },
-            {
-                featureType: 'road',
-                elementType: 'geometry',
-                stylers: [{color: '#38414e'}]
-            },
-            {
-                featureType: 'road',
-                elementType: 'geometry.stroke',
-                stylers: [{color: '#212a37'}]
-            },
-            {
-                featureType: 'road',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#9ca5b3'}]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'geometry',
-                stylers: [{color: '#746855'}]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'geometry.stroke',
-                stylers: [{color: '#1f2835'}]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#f3d19c'}]
-            },
-            {
-                featureType: 'transit',
-                elementType: 'geometry',
-                stylers: [{color: '#2f3948'}]
-            },
-            {
-                featureType: 'transit.station',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
-            },
-            {
-                featureType: 'water',
-                elementType: 'geometry',
-                stylers: [{color: '#17263c'}]
-            },
-            {
-                featureType: 'water',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#515c6d'}]
-            },
-            {
-                featureType: 'water',
-                elementType: 'labels.text.stroke',
-                stylers: [{color: '#17263c'}]
-            }
-          ]
+        document.getElementById('map'), {
+            center: upenn,
+            zoom: 16,
+            styles: [{
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#242f3e'
+                    }]
+                },
+                {
+                    elementType: 'labels.text.stroke',
+                    stylers: [{
+                        color: '#242f3e'
+                    }]
+                },
+                {
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#746855'
+                    }]
+                },
+                {
+                    featureType: 'administrative.locality',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#263c3f'
+                    }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#6b9a76'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#38414e'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{
+                        color: '#212a37'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#9ca5b3'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#746855'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{
+                        color: '#1f2835'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#f3d19c'
+                    }]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#2f3948'
+                    }]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#17263c'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#515c6d'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{
+                        color: '#17263c'
+                    }]
+                }
+            ]
+        });
+
+    places.forEach(function (place) {
+        const marker = new google.maps.Marker({
+            position: place,
+            map: map,
+        });
     });
 }
 
 function updateFactContainerWithRandomFact() {
     console.log('Getting a random fact.');
     const facts = ['On my mom\'s side of the family, I\'m the oldest of my generation.',
-    'I have two younger sisters.', 'I once won a jigsaw puzzle competition.',
-    'After graduating, I want to volunteer as CS instructor along with my full-time job.',
-    'My entire extended family immigrated to the US.',
-    'I am the oldest of all of my immediate cousins.',
-    'My first large team coding project was done with an international team.',
-    'I\'m interested in K-Pop.', 'I grew up playing with my younger cousins on a regular basis.'];
+        'I have two younger sisters.', 'I once won a jigsaw puzzle competition.',
+        'After graduating, I want to volunteer as CS instructor along with my full-time job.',
+        'My entire extended family immigrated to the US.',
+        'I am the oldest of all of my immediate cousins.',
+        'My first large team coding project was done with an international team.',
+        'I\'m interested in K-Pop.', 'I grew up playing with my younger cousins on a regular basis.'
+    ];
 
     const fact = facts[Math.floor(Math.random() * facts.length)];
     const factContainer = document.getElementById('fact-container');
-    factContainer.innerText = fact; 
+    factContainer.innerText = fact;
 }
 
 function getComments() {
@@ -141,7 +216,7 @@ function getComments() {
                     commentCount++;
                 }
             });
-    });    
+        });
 }
 
 function createCommentElement(comment) {
